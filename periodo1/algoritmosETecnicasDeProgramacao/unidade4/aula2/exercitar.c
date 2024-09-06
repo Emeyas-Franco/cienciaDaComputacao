a1563b8ad3fef0a149ce17ed3be1a668dd21d07a
Emeyas-Franco <emeyasdev@gmail.com>
2024-09-05 21:46:53 -0300

    Unidade 4 Aula 03 exercicios 01 e exemplo pratico

--- periodo1/algoritmosETecnicasDeProgramacao/unidade4/aula2/exercitar.c
+++ periodo1/algoritmosETecnicasDeProgramacao/unidade4/aula2/exercitar.c
@@ -9,14 +9,19 @@ float calcularReajuste(float salarioAtual, float percentualReajuste) {
 int main() {
     float salAual, percentReajuste, salarioNovo;
     char colaborador[50];
+
     printf("Seja bem vindo ao sistema da ABC001\n");
     printf("Digite o nome do colaborador: ");
     fgets(colaborador, sizeof(colaborador), stdin);
+
     printf("Digite o salario atual de %s " , colaborador);
     scanf("%f", &salAual);
     printf("Digite o percentual de reajuste: ");
     scanf("%f", &percentReajuste);
+
     salarioNovo = calcularReajuste(salAual, percentReajuste);
+
     printf("O colaborador %sTinha um salario de %.2f, e recebeu um reajuste de %.2f%.\nAgora seu novo salario e de R$ %.2f", colaborador, salAual, percentReajuste, salarioNovo);
+
     return 0;
 }
