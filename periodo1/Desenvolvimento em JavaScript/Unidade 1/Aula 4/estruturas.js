c3d0f5b4b7fa95a119e89c3d6bc77a4c05e8fa36
Emeyas-Franco <emeyasdev@gmail.com>
2024-10-12 09:40:08 -0300

    Unidade 1 Aula 4

--- periodo1/Desenvolvimento em JavaScript/Unidade 1/Aula 4/estruturas.js
+++ periodo1/Desenvolvimento em JavaScript/Unidade 1/Aula 4/estruturas.js
@@ -1,4 +1,36 @@
 const listCompra = ['leite', 'maça', 'iogurte'];
-listCompra
+console.log(listCompra)
+console.log(listCompra.length)
+console.log(listCompra.push('Arroz'))
+console.log(listCompra)
+console.log(listCompra.pop())
+console.log(listCompra)
+console.log(listCompra.shift())
+console.log(listCompra)
 
-listCompra.length
+
+//Função
+function subtracao(a,b){ 
+    return a-b;
+}
+//Arrow Function
+const subtrair = (a,b) => a-b; 
+
+//Função
+function soma(a) {
+    return a+2;
+}
+
+//Arrow Function
+const somar = (a) => a+2;
+
+//Função
+function diaDoMes(){
+    return new Date().getDate();
+}
+
+//Arrow Function
+const diaMes = () => new
+    Date().getDate();
+
+console.log(diaMes())
